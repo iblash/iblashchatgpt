d17b050415f14c3c4b960ed7cab99495b37ee561
Milton Lash <iblash@live.com>
2023-03-31 19:58:54 +0000

    first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def generate_response()
+      api_key = ENV.fetch("OPENAI_ACCESS_KEY NOT TOKEN:")
+      engine = "text-davinci-003"
+      prompt = "Tell me a happy story about an old supercar"
+
+      openai_client = OpenAI::Client.new(api_key: api_key, default_eng response = openai_cleint.completions(prompt: prompt, max_tokens, 4000, engine: engine
+      text = response.choices(0).text)
 end
